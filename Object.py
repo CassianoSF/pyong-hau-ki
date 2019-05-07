405656d40de8a0b563ccdd0bbf743490377f1923
CassianoSF <cassianosfranco@gmail.com>
2019-05-07 01:17:16 -0300

    ...

--- Object.py
+++ Object.py
@@ -49,41 +49,39 @@ class Object():
         self.model['scale'] = [x,y,z]
 
     def mount_mvp(self, camera):
-            trans_matrix = numpy.transpose(pyrr.matrix44.create_from_translation(self.model['translation']))
-            rot_matrix_x = numpy.transpose(pyrr.matrix44.create_from_x_rotation(self.model['rotation'][0]))
-            rot_matrix_y = numpy.transpose(pyrr.matrix44.create_from_y_rotation(self.model['rotation'][1]))
-            rot_matrix_z = numpy.transpose(pyrr.matrix44.create_from_z_rotation(self.model['rotation'][2]))
-            rot_matrix   = numpy.matmul(numpy.matmul(rot_matrix_x, rot_matrix_y),rot_matrix_z)
-            scale_matrix = numpy.transpose(pyrr.matrix44.create_from_scale(self.model['scale'] ))
-            self.model_matrix = numpy.matmul(numpy.matmul(trans_matrix,rot_matrix),scale_matrix)
+        trans_matrix = numpy.transpose(pyrr.matrix44.create_from_translation(self.model['translation']))
+        rot_matrix_x = numpy.transpose(pyrr.matrix44.create_from_x_rotation(self.model['rotation'][0]))
+        rot_matrix_y = numpy.transpose(pyrr.matrix44.create_from_y_rotation(self.model['rotation'][1]))
+        rot_matrix_z = numpy.transpose(pyrr.matrix44.create_from_z_rotation(self.model['rotation'][2]))
+        rot_matrix   = numpy.matmul(numpy.matmul(rot_matrix_x, rot_matrix_y),rot_matrix_z)
+        scale_matrix = numpy.transpose(pyrr.matrix44.create_from_scale(self.model['scale'] ))
+        self.model_matrix = numpy.matmul(numpy.matmul(trans_matrix,rot_matrix),scale_matrix)
 
-            self.view_matrix = numpy.transpose(pyrr.matrix44.create_look_at(
-                numpy.array(camera.view['position'], dtype="float32"),
-                numpy.array(camera.view['target'],   dtype="float32"),
-                numpy.array(camera.view['up'],       dtype="float32")
+        self.view_matrix = numpy.transpose(pyrr.matrix44.create_look_at(
+            numpy.array(camera.view['position'], dtype="float32"),
+            numpy.array(camera.view['target'],   dtype="float32"),
+            numpy.array(camera.view['up'],       dtype="float32")
+        ))
+
+        if camera.orthogonal:
+            self.proj_matrix = numpy.transpose(
+                pyrr.matrix44.create_orthogonal_projection_matrix(
+                    -6, 6, -3, 3, 0.001, 300, dtype=None))
+        
+        else:
+            self.proj_matrix = numpy.transpose(pyrr.matrix44.create_perspective_projection(
+                camera.projection['fovy'],
+                camera.projection['aspect'],
+                camera.projection['near'],
+                camera.projection['far'],
+                camera.projection['dtype']
             ))
-
-            if camera.orthogonal:
-                self.proj_matrix = numpy.transpose(pyrr.matrix44.create_orthogonal_projection_matrix(-6, 6, -3, 3, 0.001, 300, dtype=None))
-            
-            else:
-                self.proj_matrix = numpy.transpose(pyrr.matrix44.create_perspective_projection(
-                    camera.projection['fovy'],
-                    camera.projection['aspect'],
-                    camera.projection['near'],
-                    camera.projection['far'],
-                    camera.projection['dtype']
-                ))
-
-            # m = numpy.matmul(numpy.matmul(proj_matrix,self.view_matrix),self.model_matrix) 
-            # return numpy.transpose(m)
 
 
     def render(self, camera):
 
-        mvp = self.mount_mvp(camera)
+        self.mount_mvp(camera)
 
-        self.texture.bind()
         self.shader.add_uniform_matrix_4f("model", self.model_matrix)
         self.shader.add_uniform_matrix_4f("view", self.view_matrix)
         self.shader.add_uniform_matrix_4f("projection", self.proj_matrix)
@@ -91,7 +89,9 @@ class Object():
         self.shader.add_uniform_3f("LightPosition2", [ 20.,  20.,  20.])
         self.shader.add_uniform_3f("LightPosition3", [ 20.,  20.,  20.])
 
+        self.texture.bind()
         self.shader.bind()
         self.va.bind()
         self.ib.bind()
+    
         glDrawElements(GL_TRIANGLES, self.va.size, GL_UNSIGNED_INT, None)
